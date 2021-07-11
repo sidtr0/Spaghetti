0da2e7457ba91f5a4c014c8789d20fc191bcbcde
DracTheDino <sidtrip06@gmail.com>
2021-07-11 13:56:57 +0530

    Update Configuration.kt and remove unneeded code.

--- src/main/kotlin/me/jakejmattson/bot/commands/Suggestions.kt
+++ src/main/kotlin/me/jakejmattson/bot/commands/Suggestions.kt
@@ -4,9 +4,9 @@ import me.jakejmattson.discordkt.api.arguments.EveryArg
 import me.jakejmattson.discordkt.api.dsl.commands
 import java.awt.Color
 
-fun general() = commands("General") {
-    command("Say") {
-        description = "Make the bot say something."
+fun suggestions() = commands("Suggestions") {
+    command("Suggest") {
+        description = "Make a suggestion for the server. The suggestion will be sent to the staff of this server for review and if approved, it will be put up in a public voting channel."
         execute(EveryArg) {
             val output = args.first
             respond(output)
---
+++ src/main/kotlin/me/jakejmattson/bot/data/Configuration.kt
@@ -0,0 +1,37 @@
+package me.jakejmattson.bot.data
+
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.entity.Guild
+import dev.kord.core.entity.channel.TextChannel
+import me.jakejmattson.discordkt.api.dsl.Data
+
+data class Configuration(
+    val ownerID: String = "448143136494190592",
+    var prefix: String = "++",
+    val guildConfigurations: MutableMap<Long, GuildConfiguration> = mutableMapOf()
+) : Data("config/guild_config.json", killIfGenerated = false) {
+    fun setup (
+        guild: Guild,
+        serverPrefix: String,
+        staffReviewChannel: TextChannel,
+        publicVotingChannel: TextChannel,
+    ) {
+        if (guildConfigurations[guild.id.value] != null) return
+        val newGuildConfiguration = GuildConfiguration(
+            guild.id.value,
+            serverPrefix,
+            staffReviewChannel.id,
+            publicVotingChannel.id,
+        )
+        guildConfigurations[guild.id.value] = newGuildConfiguration
+        save()
+    }
+}
+
+data class GuildConfiguration(
+    val guildId: Long? = null,
+    var serverPrefix: String = "sp!",
+    var staffReviewChannel: Snowflake,
+    var publicVotingChannel: Snowflake,
+    val suggestions: MutableMap<Int, String> = mutableMapOf()
+)
